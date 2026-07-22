01c7672f6061d0fb78226b54bfb9513620545760
AkhilChandran <akhil@trycle.com>
2026-07-22 10:20:53 +0530

    progress included

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "./Pages/Login.jsx";
 import { Signup } from "./Pages/Signup.jsx";
 
