111c947c04af77ccb23b0a22858244f813030d94
AkhilChandran <akhil@trycle.com>
2026-07-22 10:03:28 +0530

    progress included

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { Login } from "./Pages/Login.jsx";
 import { Signup } from "./Pages/Signup.jsx";
-// import { TodoPage } from "./Pages/TodoPage.jsx";
+
 import Dashboard from "./Pages/Dashboard.jsx";
 
 export const App = () => {
@@ -11,7 +11,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        {/* <Route path="/todos" element={<TodoPage />} /> */}
+
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
